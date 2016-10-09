d7a49ca5b818f55d6a81a89cf48251372baa9bf8
Gurpreet <gurpreetsk@hotmail.com>
2016-10-09 11:14:27 +0530

    - fixed issues - Reviews and Trailers now loading

--- app/src/main/java/gurpreetsk/me/popularmovies1/DetailFragment.java
+++ app/src/main/java/gurpreetsk/me/popularmovies1/DetailFragment.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -61,14 +62,6 @@ public class DetailFragment extends Fragment {
     ArrayList<String> trailersName = new ArrayList<>();
 
     public DetailFragment() {
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-//        fetchAndSetupReviews(data.getString("id"));
-//        fetchAndSetupTrailers(data.getString("id"));
-
     }
 
     @Override
@@ -133,9 +126,11 @@ public class DetailFragment extends Fragment {
 
 
         reviewsAdapter = new ReviewsAdapter(getContext(), reviews, reviewer);
+        reviewsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         reviewsRecyclerView.setAdapter(reviewsAdapter);
 
         trailersAdapter = new TrailersAdapter(getContext(), trailers, trailersName);
+        trailersRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         trailersRecyclerView.setAdapter(trailersAdapter);
 
         return v;
